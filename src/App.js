b93dd8b8463a9ad10c45a6828c4eda3eb3467571
Francisco Egea <puertorey@gmail.com>
2020-03-27 11:12:04 +0000

    Changed database urls from localhost to Heroku

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://peaceful-stream-78196.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://peaceful-stream-78196.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}	
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://peaceful-stream-78196.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
